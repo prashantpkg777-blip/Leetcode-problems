bc760996cccf2649bade7072826406b041827b64
Prashant Kumar <prashantpkg777@gmail.com>
2026-05-09 22:39:53 +0530

    Time: 136 ms (15.08%), Space: 214.7 MB (71.43%) - LeetHub

---
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstUniqueFreq(int[] nums) {
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        //count Frequency
+        for(int n:nums){
+            freq.put(n, freq.getOrDefault(n,0)+1);
+        }
+
+        HashMap<Integer, Integer> freqCount = new HashMap<>();
+        //count frequencies of frequency
+        for (int f : freq.values()) {
+            freqCount.put(f, freqCount.getOrDefault(f, 0) + 1);
+        }
+
+        for (int n : nums) {
+            int f = freq.get(n);
+
+            if (freqCount.get(f) == 1) {
+                return n;
+            }
+        }    
+        return -1;
+    }
+}
